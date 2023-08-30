293762ab294c65df41d73a35e38fa4c7fc1eb7f1
osparking <jbpark03@gmail.com>
2023-08-30 10:51:01 +0900

    config class defines choolsoo and rookie

---
+++ src/main/java/ch03_ex1/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package ch03_ex1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import ch03_ex1.entity.Parrot;
+import ch03_ex1.entity.Person;
+
+@Configuration
+public class ProjectConfig {
+	@Bean
+	Person person() {
+		var p = new Person();
+		p.setName("철수");
+		return p;
+	}
+
+	@Bean
+	Parrot parrot() {
+		var p = new Parrot();
+		p.setName("루키");
+		return p;
+	}
+}
