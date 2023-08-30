edfd26442ebf6e4c78821875b91da0e5153356d1
osparking <jbpark03@gmail.com>
2023-08-30 10:44:16 +0900

    two main actor classes

---
+++ src/main/java/ch03_ex1/entity/Parrot.java
@@ -0,0 +1,8 @@
+package ch03_ex1.entity;
+
+import lombok.Data;
+
+@Data
+public class Parrot {
+	private String name;
+}
---
+++ src/main/java/ch03_ex1/entity/Person.java
@@ -0,0 +1,10 @@
+package ch03_ex1.entity;
+
+import lombok.Data;
+
+@Data
+public class Person {
+	private String name;
+	private Parrot parrot;
+
+}
