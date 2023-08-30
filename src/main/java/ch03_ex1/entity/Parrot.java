00cba33ee0871288130ed91b49062b992251717e
osparking <jbpark03@gmail.com>
2023-08-30 12:07:12 +0900

    toString overidden for 2 entity classes

--- src/main/java/ch03_ex1/entity/Parrot.java
+++ src/main/java/ch03_ex1/entity/Parrot.java
@@ -5,4 +5,10 @@ import lombok.Data;
 @Data
 public class Parrot {
 	private String name;
+
+	@Override
+	public String toString() {
+		return "앵무새: " + name;
+	}
+	
 }
--- src/main/java/ch03_ex1/entity/Person.java
+++ src/main/java/ch03_ex1/entity/Person.java
@@ -7,4 +7,14 @@ public class Person {
 	private String name;
 	private Parrot parrot;
 
+	@Override
+	public String toString() {
+		var sb = new StringBuffer("사람: ");
+		sb.append(name);
+		sb.append("\n");
+		sb.append("애완동물: ");
+		sb.append(parrot.toString());
+		
+		return sb.toString();
+	}
 }
